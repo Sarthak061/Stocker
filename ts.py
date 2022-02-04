7c8e328ac7f2d76b321392335f176ab6b537b92c
Sarthak Sharma <86276256+Sarthak061@users.noreply.github.com>
2022-02-04 23:20:27 +0530

    Update ts.py

--- ts.py
+++ ts.py
@@ -238,3 +238,4 @@ else:
             st.write("SMAPE of Low : {}".format(smape_low))
 dtaa = yf.download(stock, start="2010-01-01", end="2022-01-01", period = prd)
 db.close()
+"You can find the data of the above ticker [here](https://finance.yahoo.com/)."
